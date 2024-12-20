a015e9e541fba1d706ccf1fe024ec7a996f1c26e
Jiatao Geng <j29geng@uwaterloo.ca>
2024-12-20 18:46:49 -0500

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Task from "./Task";
+
 export default function App() {
   return (
     <>
       <h1> Hello There </h1>
-      <input type="text" id="task"/>
+      <input type="text" id="task" placeholder="Enter task"/>
       <button className="addTask">Add</button>
+      <ul className = "taskList">
+        <Task />
+        <Task />
+        <Task />
+      </ul>
     </>
   )
 }
