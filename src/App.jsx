81382bf7aa47a5703cca49257a33a8692f4352a4
Jiatao Geng <j29geng@uwaterloo.ca>
2024-12-20 18:49:09 -0500

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Task from "./Task";
 export default function App() {
   return (
     <>
-      <h1> Hello There </h1>
+      <h1 className="heading">To-Do List</h1>
       <input type="text" id="task" placeholder="Enter task"/>
       <button className="addTask">Add</button>
       <ul className = "taskList">
