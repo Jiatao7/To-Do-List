d1985d099aeee869a8f09c47eef829184f9acf4b
Jiatao Geng <j29geng@uwaterloo.ca>
2024-12-20 17:13:44 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <h1> Hello There </h1>
+      <input type="text" id="task"/>
+      <button className="addTask">Add</button>
+    </>
+  )
+}
+
