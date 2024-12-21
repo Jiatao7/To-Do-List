362309f3389438f5c5b5b92eecc25f8f50c79bb1
Jiatao Geng <j29geng@uwaterloo.ca>
2024-12-20 19:22:04 -0500

    add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ export default function App() {
   return (
     <>
       <h1 className="heading">To-Do List</h1>
-      <input type="text" id="task" placeholder="Enter task"/>
-      <button className="addTask">Add</button>
+      <div className="container">
+        <input type="text" id="task" placeholder="Enter task"/>
+        <button className="addButton">Add</button>
+      </div>
       <ul className = "taskList">
         <Task />
         <Task />
