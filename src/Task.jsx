a1b31864d522297a045095b68a12d529a9d56f74
Jiatao Geng <j29geng@uwaterloo.ca>
2024-12-20 19:46:54 -0500

    add task icons

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,6 +1,16 @@
+import checked from './assets/checked.png';
+import unchecked from './assets/unchecked.png';
+import cross from './assets/cross.png';
+
+
 export default function Task() {
     return (
-        <li>A task</li>
+        <li>
+            <img className="checkbox" src={unchecked} alt="checked" />
+            <span>A task</span>
+            <img className = "cross" src={cross} alt="cross" />
+            
+        </li>
     )
 }
 
